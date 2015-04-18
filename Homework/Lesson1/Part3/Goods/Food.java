198a42382cfdf2632013c3e73f7d39f384a0f155
baymn <evgenii.kotikov@gmail.com>
2015-04-18 13:44:06 +0300

    Lesson1_Part3_drop6

---
+++ Homework/Lesson1/Part3/Goods/Food.java
@@ -0,0 +1,16 @@
+public class Food extends Goods {
+        
+    // the Food subclass has
+    // next additional fields
+    public int fats; //Year growth for a tree
+	public int proteins; //
+	public int carbs; //
+	public String taste; //
+	public String eatWay; // DP we need to cook this kind of food
+
+    // the Food subclass has
+    // next additional methods
+private getComposition(return fats, proteins, carbs);
+private getTaste(return taste); 	
+
+}
---
+++ Homework/Lesson1/Part3/Goods/HouseChemical.java
@@ -0,0 +1,14 @@
+public class HouseChemical extends Goods {
+        
+    // the HouseChemical subclass has
+    // next additional fields
+    public int levelToxicity; // toxicity level - what action we need to do in case eat or touch etc.
+	public String applicableFor; //applicable for some stuff like kitchen, bath etc.
+	public String activeAgent; // Active agent, substance, 
+
+    // the HouseChemical subclass has
+    // next additional methods
+private getlevelToxicity(return levelToxicity);
+private getApplicableFor(return applicableFor); 	
+
+}
