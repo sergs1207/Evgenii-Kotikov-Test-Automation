51262f357d3cc45b82a3de671aecc9b51ee9571d
baymn <evgenii.kotikov@gmail.com>
2015-04-19 14:42:55 +0300

    test

---
+++ Homework/Lesson1/Part3/Goods/AnimalFood.java
@@ -0,0 +1,13 @@
+public class AnimalFood extends Goods {
+        
+    // the AnimalFood subclass has
+    // next additional fields
+	public String animalType; // Dogs, cats etc.
+	public String foodType; // Dry, canned, seeds etc.
+
+    // the AnimalFood subclass has
+    // next additional methods
+private getFoodType(){return foodType};//
+private getAnimalType()(return animalType); //	
+
+}
